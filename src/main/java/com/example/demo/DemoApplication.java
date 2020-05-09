e49e33812ddff50b4012cbb2a99117fdfb0b4b1f
chengeng1107 <chengeng1107@163.com>
2020-05-09 20:58:13 +0800

    commit.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,9 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author chengeng
+ */
 @SpringBootApplication
 public class DemoApplication {
 
--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,19 +1,27 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+/**
+ * @author chengeng
+ */
 @Controller
 public class TestController {
 
-    // This is the first change in local IDE.
-    // This is the first change in github website.
+    /**
+     *  This is the first change in local IDE.
+     *  This is the first change in github website.
+      */
     @RequestMapping(value = "/helloWorld", method = RequestMethod.GET)
     @ResponseBody
-    public String HelloWorld() {
+    public String helloWorld() {
         return "Hello World!";
+    }
+
+    @GetMapping(value = "/getMapping")
+    @ResponseBody
+    public String testGetMapping() {
+        return "testGetMapping";
     }
 }
